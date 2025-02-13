c87620e39cac719ce26237f85ee9017b35f8a1e7
rubberduckster <mcrubygirl@hotmail.com>
2025-02-13 14:52:30 +0100

    Finished the main menu

--- C Sharp Practice/Program.cs
+++ C Sharp Practice/Program.cs
@@ -23,10 +23,25 @@
             Console.WriteLine("7. Switch cases --------------------------------------- |");
             Console.WriteLine("8. Loops ---------------------------------------------- |");
             Console.WriteLine("9. Extended control structure ------------------------- |");
+            Console.WriteLine("10. Methods and instances ----------------------------- |");
+            Console.WriteLine("11. Return types and parameters ----------------------- |");
+            Console.WriteLine("12. Instance variables -------------------------------- |");
+            Console.WriteLine("13. Inheritance --------------------------------------- |");
+            Console.WriteLine("14. Constructors -------------------------------------- |");
+            Console.WriteLine("15. Arrays -------------------------------------------- |");
+            Console.WriteLine("16. Loop of array ------------------------------------- |");
+            Console.WriteLine("16. Public, private and protected --------------------- |");
             Console.WriteLine("");
             Console.WriteLine("Type one of the numbers in the menu to view the assingment.");
             Console.WriteLine("");
             Console.Write("Enter number: ");
+
+            switch (Console.ReadLine()) {
+                case "1":
+                    Console.Clear();
+                    Console.WriteLine();
+                    break;
+            }
 
             Console.ReadKey();
             //do { } while ();
